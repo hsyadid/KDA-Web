0acdec6a13b26091e3d732042b95a096966f73d9
VazhaK14 <khayrivazhasocial@gmail.com>
2025-05-14 22:25:47 +0700

    feat: about us

---
+++ modules/AboutUsModule/components/Shadow.tsx
@@ -0,0 +1,9 @@
+const Shadow = () => {
+  return (
+    <>
+      <div className="absolute w-xl h-52 bg-[#194B49] blur-xl opacity-50 rounded-full -z-10" />
+    </>
+  );
+};
+
+export default Shadow;

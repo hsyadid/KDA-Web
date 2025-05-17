d6d44e1c6dbc2504edfe65a9991ded5f43eb9b6c
VazhaK14 <khayrivazhasocial@gmail.com>
2025-05-18 03:04:46 +0700

    feat: 3d card components

---
+++ app/components/CardRight.tsx
@@ -0,0 +1,15 @@
+const CardRight = () => {
+  return (
+    <div className="flex items-center perspective-near perspective-center justify-center min-h-screen bg-gray-100">
+      <div className="relative rotate-y-10 transform transform-style-3d">
+        <div className="w-80 h-96 bg-white rounded-lg shadow-lg rotate-y-45">
+          <div className="p-6">
+            <p className="mt-4 text-gray-600">SIGMA 3d CSS 🔥🔥🔥</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardRight;
